bf3a4b78895f54f4d5e8955ab7bd4fdc0d2a64fe
hesauhugo <hesau.hugo@hotmail.com>
2023-05-28 10:05:28 -0300

    feat: object

---
+++ Models/Computador.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Threading.Tasks;
+
+namespace DotNet_Classes_Abstratas_Interfaces.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Computador :Object
+    {
+        public override string ToString(){
+            return "Método ToString() sobrescrito";
+        }
+
+    }
+}
